91f8960477e8e8398cc9db06863d5abd5169262c
Francky <fitiafrancky10@gmail.com>
2026-02-10 14:19:14 +0300

    Docker final

--- frontend-web/src/app/models/historique.models.ts
+++
@@ -1,9 +0,0 @@
-export interface HistoriqueDto {
-  id: number;
-  signalementId: number;
-  managerId: number;
-  managerNom: string;
-  action: string;
-  details: string;
-  date: string;
-}
--- frontend-web/src/app/models/stats.models.ts
+++
@@ -1,9 +0,0 @@
-export interface StatsDto {
-  nbPoints: number;
-  totalSurfaceM2: number;
-  avancementPercent: number;
-  totalBudget: number;
-  nbNouveau: number;
-  nbEnCours: number;
-  nbTermine: number;
-}
